a3536ad5915b3648a209dcdfb4c78338182ad7f7
chamalkaMarasinghe <chamalkamarasinghe11@gmail.com>
2025-11-27 03:47:43 +0530

    initial comit client

---
+++ src/pages/ClientHome.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ClientHome = () => {
+  return (
+    <>
+      <div className="max-w-4xl px-6 py-10">
+        <h1 className="mt-24 mb-4 text-4xl font-bold text-left text-primary">
+          Welcome to my
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default ClientHome;
